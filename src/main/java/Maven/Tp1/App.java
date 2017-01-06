29e5bbc7d8c34decee283c908ade5b3d1355575c
17013859 <17013859@e010m09.istic.univ-rennes1.fr>
2017-01-06 12:16:07 +0100

    errreur de cobortura

---
+++ src/main/java/Maven/Tp1/App.java
@@ -0,0 +1,27 @@
+package Maven.Tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+
+
+
+{
+	
+	 int b=5;
+     
+     double a=5;
+     boolean c= true;
+     
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+       
+
+    }
+    
+    
+}
